7923b7d6d5b4448e5899dc6ae7b6176688f95f9d
Josah <josah.weber@web.de>
2018-10-17 20:10:40 +0200

    Add Supertest

--- Kuester/OberverSimple/src/com/example/observer/Beobachter.java
+++
@@ -1,7 +0,0 @@
-package com.example.observer;
-
-public interface Beobachter {
-
-	public void aktualisieren(Subjekt subjekt);
-	
-}
--- Kuester/OberverSimple/src/com/example/observer/Subjekt.java
+++
@@ -1,8 +0,0 @@
-package com.example.observer;
-
-public interface Subjekt {
-
-	public void registriereBeobachter(Beobachter b);
-	public void entferneBeobachter(Beobachter b);
-	public String gibZustand();
-}
