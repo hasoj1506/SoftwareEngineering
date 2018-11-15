355d4bfb1043c6b227c5568c60779a89ec086efd
Josah <josah.weber@web.de>
2018-11-15 14:38:51 +0100

    gui

---
+++ SingletonShipMarket/src/shipmarketExample/ShipMarketGUI.java
@@ -0,0 +1,74 @@
+package shipmarketExample;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.JScrollBar;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JButton;
+
+public class ShipMarketGUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ShipMarketGUI frame = new ShipMarketGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ShipMarketGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel lblThisIsA = new JLabel("This is a ShipMarket");
+		contentPane.add(lblThisIsA, BorderLayout.NORTH);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"Ship", "ShipOwner", "To sell", "Interested"
+			}
+		));
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		contentPane.add(table, BorderLayout.CENTER);
+		
+		JButton btnFillTable = new JButton("Fill Table");
+		contentPane.add(btnFillTable, BorderLayout.SOUTH);
+	}
+
+}
